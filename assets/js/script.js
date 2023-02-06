d4052855987c43855a693f618d884e69f200f61f
Chinara0303 <chinaraei@code.edu.az>
2023-02-06 17:21:39 +0400

    Task is ready

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+'use strict';
+//datatypes var,let,const
+// var y = 55;
+// var y = 89;
+// console.log(y);
+
+// let a=22;
+// a=3;
+// console.log(a);
+
+// const IS_MARRIED = true;
+// IS_MARRIED = false //error
+// console.log(IS_MARRIED);
+
+// alert('Hello Chinara');
+
+// let username = prompt('Enter a username');
+// console.log(username);
+
+// let isOnline = confirm('Is she online?')
+// console.log(isOnline);
+
+//primitive non-primitive types 
+ let age = 22;
+  let firstName = 'Chinara';
+// let address = 'Lok';
+// let isMarried = false;
+// let isOnline;
+// let height = null;
+
+// if(age===firstName){
+//     console.log("cc");
+// }
+// else{
+//     console.log("dd");
+// }
+
+// console.log(typeof age);
+// console.log(typeof firstName);
+// console.log(typeof address);
+// console.log(typeof isMarried);
+// console.log(typeof isOnline);
+// console.log(typeof height);
+
+//  let isMarried = true;
+// if (isMarried) {
+//     console.log("Evlidir");
+// }
+// else{
+//     console.log("Subaydir");
+// }
+
+// let result =  isMarried ? "Evlidir" : "Subaydir";
+// console.log(result);
+
+// let students = ["Chinara","Konul","Roya",22,26,true];
+// let ages = [22, 26, 28];
+// for (let i = 0; i < ages.length; i++) {
+//     if (ages[2] > 20) {
+//         console.log(ages[2]);
+//         break;
+//     }
+// }
+
+// let address = null;
+// let res = address?? "Lok";
+// console.log(res);
+
+// let x= 5;
+// let y=3;
+// if(x%=y){
+//     console.log(x);
+// }
+// console.log(x+y);
+// console.log(x**y);
+// console.log(x-y);
+// console.log(x/y);
+// console.log(x%y);
+// let y=8;
+// let Y =9;
+// console.log(y);
+// console.log(Y);
